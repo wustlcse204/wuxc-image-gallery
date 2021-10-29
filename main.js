e03a7ca856b44674d4e41374b660322a450cbbaf
chentiffany <c.tiffany@wustl.edu>
2021-10-29 16:54:14 -0500

    changes

--- main.js
+++ main.js
@@ -1,46 +1,51 @@
-var slideIndex = 0;
-var expandImg = null;
-var allImages = null;
-var lastItem = null;
-var firstItem = null;
+//initializing variables
+var currentImage = 0;
+var enlargeImage = null;
+var imageGallery = null;
+var lastPic = null;
+var firstPic = null;
 
-function enlarge(imgs) {
-    expandImg = document.getElementById('expandedImg');
-    slideIndex = parseInt(imgs.getAttribute("data-index"));
-    expandImg.src = imgs.src;
-    document.getElementById("enlarged-box").style.display = "block";
-    console.log(slideIndex)
-    allImages = document.getElementsByClassName('image');
-    lastItem = allImages[allImages.length - 1];
-    firstItem = allImages[0];
-    console.log(allImages);
-    document.getElementById("next").addEventListener("click", nextSlide);
+//enlarging the photos
+function makeBig(imgs) {
+    enlargeImage = document.getElementById('bigImage');
+    currentImage = parseInt(imgs.getAttribute("idx"));
+    enlargeImage.src = imgs.src;
+    document.getElementById("big-pic").style.display = "block";
+    console.log(currentImage)
+    imageGallery = document.getElementsByClassName('image');
+    lastPic = imageGallery[imageGallery.length - 1];
+    firstPic = imageGallery[0];
+    console.log(imageGallery);
+    document.getElementById("next").addEventListener("click", next);
     document.getElementById("previous").addEventListener("click", previous);
 
 }
 
-function closeImage(close) {
-    document.getElementById("enlarged-box").style.display = "none";
+//closing the focused/enlarged image
+function closeBig(close) {
+    document.getElementById("big-pic").style.display = "none";
 }
 
+//going to the image from before
 function previous() {
-    slideIndex -= 1;
-    if (slideIndex <= 0) {
-        expandImg.src = lastItem.src;
-        slideIndex = allImages.length - 1;
-        console.log(slideIndex)
+    currentImage -= 1;
+    if (currentImage <= 0) {
+        enlargeImage.src = lastPic.src;
+        currentImage = imageGallery.length - 1;
+        console.log(currentImage)
     } else {
-        expandImg.src = allImages[slideIndex].src;
+        enlargeImage.src = imageGallery[currentImage].src;
     }
 }
 
-function nextSlide() {
-    slideIndex += 1;
-    if (slideIndex == allImages.length) {
-        expandImg.src = firstItem.src;
-        slideIndex = 0;
-        console.log(slideIndex);
+//going to the next image
+function next() {
+    currentImage += 1;
+    if (currentImage == imageGallery.length) {
+        enlargeImage.src = firstPic.src;
+        currentImage = 0;
+        console.log(currentImage);
     } else {
-        expandImg.src = allImages[slideIndex].src;
+        enlargeImage.src = imageGallery[currentImage].src;
     }
 }
