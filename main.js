33dc75d1399fef1c815b3191ca64d6078dc32d33
chentiffany <c.tiffany@wustl.edu>
2021-10-29 16:04:17 -0500

    changes

---
+++ main.js
@@ -0,0 +1,46 @@
+var slideIndex = 0;
+var expandImg = null;
+var allImages = null;
+var lastItem = null;
+var firstItem = null;
+
+function enlarge(imgs) {
+    expandImg = document.getElementById('expandedImg');
+    slideIndex = parseInt(imgs.getAttribute("data-index"));
+    expandImg.src = imgs.src;
+    document.getElementById("enlarged-box").style.display = "block";
+    console.log(slideIndex)
+    allImages = document.getElementsByClassName('image');
+    lastItem = allImages[allImages.length - 1];
+    firstItem = allImages[0];
+    console.log(allImages);
+    document.getElementById("next").addEventListener("click", nextSlide);
+    document.getElementById("previous").addEventListener("click", previous);
+
+}
+
+function closeImage(close) {
+    document.getElementById("enlarged-box").style.display = "none";
+}
+
+function previous() {
+    slideIndex -= 1;
+    if (slideIndex <= 0) {
+        expandImg.src = lastItem.src;
+        slideIndex = allImages.length - 1;
+        console.log(slideIndex)
+    } else {
+        expandImg.src = allImages[slideIndex].src;
+    }
+}
+
+function nextSlide() {
+    slideIndex += 1;
+    if (slideIndex == allImages.length) {
+        expandImg.src = firstItem.src;
+        slideIndex = 0;
+        console.log(slideIndex);
+    } else {
+        expandImg.src = allImages[slideIndex].src;
+    }
+}
