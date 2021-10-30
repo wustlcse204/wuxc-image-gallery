bd2759ad29454e461f4cc4f566b3056931c7b5f1
chentiffany <c.tiffany@wustl.edu>
2021-10-29 19:34:49 -0500

    done

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
-var slideIndex = 0;
+var currentImg = 0;
 var expandImg = null;
-var allImages = null;
-var lastItem = null;
-var firstItem = null;
+var imgGallery = null;
+var lastImg = null;
+var firstImg = null;
 
-function enlarge(imgs) {
+function big(imgs) {
     expandImg = document.getElementById('expandedImg');
-    slideIndex = parseInt(imgs.getAttribute("idx"));
+    currentImg = parseInt(imgs.getAttribute("idx"));
     expandImg.src = imgs.src;
     document.getElementById("enlarged-section").style.display = "block";
-    console.log(slideIndex)
-    allImages = document.getElementsByClassName('image');
-    lastItem = allImages[allImages.length - 1];
-    firstItem = allImages[0];
-    console.log(allImages);
-    document.getElementById("next").addEventListener("click", nextSlide);
+    console.log(currentImg)
+    imgGallery = document.getElementsByClassName('image');
+    lastImg = imgGallery[imgGallery.length - 1];
+    firstImg = imgGallery[0];
+    console.log(imgGallery);
+    document.getElementById("next").addEventListener("click", next);
     document.getElementById("previous").addEventListener("click", previous);
 
 }
 
-function closeImage(close) {
+function closeImage(closeBig) {
     document.getElementById("enlarged-section").style.display = "none";
 }
 
 function previous() {
-    slideIndex -= 1;
-    if (slideIndex <= 0) {
-        expandImg.src = lastItem.src;
-        slideIndex = allImages.length - 1;
-        console.log(slideIndex)
+    currentImg -= 1;
+    if (currentImg <= 0) {
+        expandImg.src = lastImg.src;
+        currentImg = imgGallery.length - 1;
+        console.log(currentImg)
     } else {
-        expandImg.src = allImages[slideIndex].src;
+        expandImg.src = imgGallery[currentImg].src;
     }
 }
 
-function nextSlide() {
-    slideIndex += 1;
-    if (slideIndex == allImages.length) {
-        expandImg.src = firstItem.src;
-        slideIndex = 0;
-        console.log(slideIndex);
+function next() {
+    currentImg += 1;
+    if (currentImg == imgGallery.length) {
+        expandImg.src = firstImg.src;
+        currentImg = 0;
+        console.log(currentImg);
     } else {
-        expandImg.src = allImages[slideIndex].src;
+        expandImg.src = imgGallery[currentImg].src;
     }
 }
